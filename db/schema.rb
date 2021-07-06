f7282a92d8031a1d788c7b667aff529b4738b063
Ivan Korney <ikorney@phoenixit.ru>
2021-07-06 15:15:53 +0300

    add: UsersController new, UserModel

--- db/schema.rb
+++ db/schema.rb
@@ -10,5 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_02_071529) do
+ActiveRecord::Schema.define(version: 2021_07_06_094222) do
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
 end
